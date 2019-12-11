3c355d1deaeb4bf034a4a3a58e909fe673bc5660
Gabriel Nader <gabrielgnader@gmail.com>
2019-12-11 19:07:51 -0300

    state and form ready

--- src/App.js
+++ src/App.js
@@ -3,11 +3,49 @@ import './css/style.css';
 
 class App extends Component {
 
-  sendAddress = () => {
-    console.log('enviou');
+  constructor() {
+    super();
+    this.sendAddress = this.sendAddress.bind(this);
+    this.setAddress = this.setAddress.bind(this);
+    
+    this.state = {
+      address:''
+      // error: null,
+      // isLoaded: false,
+      // items: []
+    };
+    
+  }
+
+  setAddress(evento) {
+    this.setState({ address: evento.target.value });
+    console.log(this.state.address)
+  }
+
+  sendAddress = (event) => {
+    event.preventDefault();
+    console.log(this.state.address);
+    // fetch("http//localhost:8080/api/autores")
+    //   .then(res => res.json())
+    //   .then(
+    //     (result) => {
+    //       this.setState({
+    //         isLoaded: true,
+    //         items: result.items
+    //       });
+    //     },
+    //     (error) => {
+    //       this.setState({
+    //         isLoaded: true,
+    //         error
+    //       });
+
+    //     }
+    //   )
   }
 
   render() {
+    // console.log(this.props)
     return (
       <div className="App">
         <header className="App-header">
@@ -15,8 +53,10 @@ class App extends Component {
       </header>
 
         <main>
-          <input type="text" name="address" id="address-input" />
-          <input type="button" value="OK" onClick={this.sendAddress.bind(this)}/>
+          <form onSubmit={this.sendAddress} method="post">
+            <input type="text" value={this.state.address} onChange={this.setAddress} />
+            <input type="submit" value="OK"/>
+          </form>
         </main>
 
         <footer>
