ce5c9ae4804807bcacd144f6855e40704341947f
Gabriel Nader <gabrielgnader@gmail.com>
2019-12-11 22:04:09 -0300

    fetch apis of the first page

--- src/App.js
+++ src/App.js
@@ -7,65 +7,104 @@ class App extends Component {
     super();
     this.sendAddress = this.sendAddress.bind(this);
     this.setAddress = this.setAddress.bind(this);
-    
+
     this.state = {
-      address:''
-      // error: null,
-      // isLoaded: false,
-      // items: []
+      address: '',
+      error: null,
+      loading: false,
+      items: null
     };
-    
   }
 
-  setAddress(evento) {
-    this.setState({ address: evento.target.value });
-    console.log(this.state.address)
+  setAddress(event) {
+    this.setState({ address: event.target.value });
   }
 
   sendAddress = (event) => {
     event.preventDefault();
-    console.log(this.state.address);
-    // fetch("http//localhost:8080/api/autores")
-    //   .then(res => res.json())
-    //   .then(
-    //     (result) => {
-    //       this.setState({
-    //         isLoaded: true,
-    //         items: result.items
-    //       });
-    //     },
-    //     (error) => {
-    //       this.setState({
-    //         isLoaded: true,
-    //         error
-    //       });
+    this.setState({ loading: true });
+    const googleUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=" + this.state.address.split(' ').join('+') + "&key=" + process.env.REACT_APP_GOOGLE_KEY;
+    console.log(googleUrl)
+    fetch(googleUrl)
+      // API GOOGLE
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result.results[0].geometry.location.lat, result.results[0].geometry.location.lng);
+          fetch("http://cdc-react.herokuapp.com/api/autores")
+            // API POC
+            .then(res => res.json())
+            .then(
+              (result) => {
+                const pocMocado = {
+                  "data": {
+                    "pocSearch": [
+                      {
+                        "__typename": "POC",
+                        "id": "532",
+                        "status": "AVAILABLE",
+                        "tradingName": "Distribuidor de Treinamento",
+                        "officialName": "Distribuidor de Treinamento"
+                      }
+                    ]
+                  }
+                }
+                console.log(pocMocado.data.pocSearch[0].id, pocMocado.data.pocSearch[0].status);
+                fetch("products.json")
+                  // API Products
+                  .then(res => res.json())
+                  .then(
+                    (result) => {
+                      this.setState({
+                        loading: false,
+                        items: result
+                      });
+                    }
+                  )
+              },
+              (error) => {
+                this.setState({
+                  loading: false,
+                  error
+                });
+              })
+        },
+        (error) => {
+          this.setState({
+            loading: false,
+            error
+          });
 
-    //     }
-    //   )
+        }
+      )
   }
 
   render() {
-    // console.log(this.props)
+    if (this.state.loading) {
+      return (
+        <div>Loading...</div>
+      );
+    }
+
+    if (this.state.items) {
+      const items = this.state.items.data.poc.products.map(function (product) {
+        return <div>{product.title}</div>
+      })
+
+      return (
+        <div>{items}</div>
+        );
+    }
+
     return (
       <div className="App">
-        <header className="App-header">
-          Header
-      </header>
-
-        <main>
-          <form onSubmit={this.sendAddress} method="post">
-            <input type="text" value={this.state.address} onChange={this.setAddress} />
-            <input type="submit" value="OK"/>
-          </form>
-        </main>
-
-        <footer>
-          copyright@2019
-      </footer>
+        <form onSubmit={this.sendAddress} method="post">
+          <input type="text" value={this.state.address} onChange={this.setAddress} />
+          <input type="submit" value="OK" />
+        </form>
       </div>
     );
   }
 }
-
 
 export default App;
