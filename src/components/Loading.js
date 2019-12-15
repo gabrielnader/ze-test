25f10457d3489832f227ca8565184a199d9749c9
Gabriel Nader <gabrielgnader@gmail.com>
2019-12-15 17:34:27 -0300

    Loading style

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import loadingImage from '../img/loading-beer.gif';
+import styled from 'styled-components';
+
+const LoadingImg = styled.img.attrs(props => ({
+  src: props.src,
+  alt: props.alt
+}))`
+  width: 100%;
+`
 
 class Loading extends Component{
   render(){
     return (
-      <div>Loading...</div>
+      <div>
+        <LoadingImg src={loadingImage} alt="Carregando produtos" ></LoadingImg>
+      </div>
     );
   }
 }
